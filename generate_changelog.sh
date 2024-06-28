281222da314d78d80dcdb4b1f579272774e20d1c
Dessalines <tyhou13@gmx.com>
2024-06-28 15:54:28 -0400

    First commit.

---
+++ generate_changelog.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# Creating the new tag
+new_tag="$1"
+
+# Writing to the Releases.md asset that's loaded inside the app
+git cliff --unreleased --tag "$new_tag" --output app/src/main/assets/RELEASES.md
+
+# Prepending to the RELEASES.md
+git cliff --tag "$new_tag" --output RELEASES.md
+prettier -w RELEASES.md app/src/main/assets/RELEASES.md
